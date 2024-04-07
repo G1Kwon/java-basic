007c002deb186c1202d0209381df8eaac213197f
g1kwon <spring_shot@naver.com>
2024-04-08 06:21:44 +0900

    Construct - Overloading + this

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,16 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    MemberConstruct(String name, int age) {
+        //방법1
+/*        this.name = name;
+        this.age = age;
+        this.grade = 50;*/
+        //방법2
+        this(name, age, 50);
+    }
+
     MemberConstruct(String name, int age, int grade) {
         //생성자는 메서드와 비슷하지만 아래와 같은 차이가 있다.
         //생성자의 이름은 클래스의 이름과 같아야 한다. 따라서 첫 글자도 대문자로 시작한다.
